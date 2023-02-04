0fc21eafc8147822b3c43aa7e397d250a6260a5a
NikhilD <email.nikhil.agarwal@gmail.com>
2023-02-04 09:03:57 -0500

    driver class

--- src/group/RunProject1.java
+++ src/group/RunProject1.java
@@ -1,7 +1,15 @@
 package group;
 
-/*@author Nikhil Agarwal*/
+/**
+ * Driver class for our Roster Project.
+ * Contains main method.
+ */
 public class RunProject1 {
+
+    /**
+     * main method for project.
+     * @param args Null.
+     */
     public static void main(String[] args){
         new RosterManager().run();
 
