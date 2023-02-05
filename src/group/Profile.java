c9e2ea049b2361d7e1bd623eeef315bf27946389
NikhilD <email.nikhil.agarwal@gmail.com>
2023-02-05 00:21:53 -0500

    Profile.java comment blocks

--- src/group/Profile.java
+++ src/group/Profile.java
@@ -1,11 +1,23 @@
 package group;
 
-/*@author Nikhil Agarwal*/
+/**
+ * Class to implement Profile Object.
+ * Holds LastName, FirstName, and DoB as parameters.
+ * Contains methods to fetch profile values from outside this class,
+ * as well as methods that override equals, toString, and compareTo.
+ * @author Nikhil Agarwal
+ */
 public class Profile implements Comparable<Profile>{
     private String lname;
     private String fname;
     private Date dob;
 
+    /**
+     * Constructor for Profile Object.
+     * @param lname Last Name of profile.
+     * @param fname First Name of profile.
+     * @param dob DoB of profile.
+     */
     public Profile(String lname, String fname, Date dob){
         this.lname = lname;
         this.fname = fname;
@@ -33,11 +45,20 @@ public class Profile implements Comparable<Profile>{
         return dob;
     }
 
+    /**
+     * Overrides toString method from Java Object class.
+     * @return Profile as string printed as: firstName lastName doB
+     */
     @Override
     public String toString(){
         return fname+" "+lname+" "+dob.toString();
     }
 
+    /**
+     * Overrides equals method from Java Object class.
+     * @param obj object to be checked.
+     * @return true if object equals Profile, false if otherwise.
+     */
     @Override
     public boolean equals(Object obj){
         Profile profile = (Profile) obj;
@@ -51,6 +72,11 @@ public class Profile implements Comparable<Profile>{
         return false;
     }
 
+    /**
+     * Overrides compareTo method from Java Comparable class.
+     * @param profile the object to be compared.
+     * @return
+     */
     @Override
     public int compareTo(Profile profile){
         int compareLastName = lname.compareTo(profile.lname);
